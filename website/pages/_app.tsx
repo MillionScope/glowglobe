2eb8efdc12cf803089f8c17bf3fe3170beb20da2
ThanhHM2 <thanhhm2@vng.com.vn>
2023-10-22 00:57:00 +0700

    update package

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles.css"
 import "nextra-theme-docs/style.css"
 import Head from "next/head"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Nextra({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
   return getLayout(
     <>
+      <Head>{}</Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
