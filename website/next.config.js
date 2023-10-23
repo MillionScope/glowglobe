cf6b5e61f4d5db98a29f3ff3353756a60994cec1
ThanhHM2 <thanhhm2@vng.com.vn>
2023-10-23 15:41:36 +0700

    update tailwindcss

--- website/next.config.js
+++ website/next.config.js
@@ -3,6 +3,25 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
   unstable_flexsearch: true,
   unstable_staticImage: true,
+  plugins: [
+    'tailwindcss',
+    'postcss-flexbugs-fixes',
+    'postcss-preset-env',
+    [
+      'postcss-normalize',
+      {
+        allowDuplicates: false,
+      },
+    ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      },
+    ],
+    'autoprefixer',
+  ],
 })
 
 module.exports = withNextra()
